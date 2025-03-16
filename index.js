0eb74fe161436b8625c507dc1806445d0b4a4352
AnanyaCherukuri05 <ananyachrukuri5@gmail.com>
2025-03-16 13:57:54 +0530

    .

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require('express');
-const { resolve } = require('path');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const menuRoutes = require('./routes/menu');
 
 const app = express();
-const port = 3010;
+const port = process.env.PORT || 3000;
+app.use(bodyParser.json());
+mongoURI='mongodb+srv://ananyacherukuri5:tG2aoHvSbZHusZ3e@cluster0.tjmvg.mongodb.net/data?retryWrites=true&w=majority&appName=Cluster0';
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
 
-app.use(express.static('static'));
-
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
-});
+app.use('/menu', menuRoutes);
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Server running on port http://localhost:${port}`);
 });
